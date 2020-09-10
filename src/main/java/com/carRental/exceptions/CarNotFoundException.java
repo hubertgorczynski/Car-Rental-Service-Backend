fa22e161b2e93e2f4db6e93c18db42bbf00ed5bb
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-09-10 21:50:48 +0200

    Added @ResponseStatus annotation for exception handling classes

--- src/main/java/com/carRental/exceptions/CarNotFoundException.java
+++ src/main/java/com/carRental/exceptions/CarNotFoundException.java
@@ -1,4 +1,8 @@
 package com.carRental.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CarNotFoundException extends Exception {
 }
