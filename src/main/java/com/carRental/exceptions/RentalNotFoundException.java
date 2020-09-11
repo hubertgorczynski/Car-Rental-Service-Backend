96c13a35766cfa5b4db27cfd2e0a935f22d4d9b0
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-09-11 12:37:26 +0200

    Modified Exception handling class with @ResponseStatus annotation

--- src/main/java/com/carRental/exceptions/RentalNotFoundException.java
+++ src/main/java/com/carRental/exceptions/RentalNotFoundException.java
@@ -3,6 +3,6 @@ package com.carRental.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Rental Not Found")
 public class RentalNotFoundException extends Exception {
 }
--- src/main/java/com/carRental/exceptions/UserNotFoundException.java
+++ src/main/java/com/carRental/exceptions/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.carRental.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User Not Found")
 public class UserNotFoundException extends Exception {
 }
