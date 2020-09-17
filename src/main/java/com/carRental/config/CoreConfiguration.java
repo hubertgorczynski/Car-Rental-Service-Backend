6108e1668815a1def3642b6db98a3ca59c61ca91
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-09-17 19:48:37 +0200

    Create textJsonConverter for fix error with email verification API

--- src/main/java/com/carRental/config/CoreConfiguration.java
+++ src/main/java/com/carRental/config/CoreConfiguration.java
@@ -2,14 +2,24 @@ package com.carRental.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @EnableScheduling
 @Configuration
 public class CoreConfiguration {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        MappingJackson2HttpMessageConverter textJsonConverter = new MappingJackson2HttpMessageConverter();
+        textJsonConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN));
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(textJsonConverter);
+
+        return restTemplate;
     }
 }
