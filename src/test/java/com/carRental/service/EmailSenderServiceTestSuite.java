a336a64074b9043d1fa78016c20c398564a22101
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-09-27 22:49:38 +0200

    Create test for EmailSenderService

---
+++ src/test/java/com/carRental/service/EmailSenderServiceTestSuite.java
@@ -0,0 +1,41 @@
+package com.carRental.service;
+
+import com.carRental.domain.Mail;
+import com.carRental.service.emailService.EmailSenderService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailSenderServiceTestSuite {
+
+    @InjectMocks
+    private EmailSenderService emailSenderService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail() {
+        //Given
+        Mail mail = new Mail("email@test.com", "ccemail@test.com", "subject", "message");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setCc((mail.getToCc()));
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        //When
+        emailSenderService.sendMail(mail);
+
+        // Then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+}
